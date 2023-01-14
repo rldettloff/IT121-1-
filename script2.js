4f322cbc54078f9dc68a3a9c5c48e2bb4f1e790f
rldettloff <114546927+rldettloff@users.noreply.github.com>
2023-01-14 03:18:54 +0000

    Assigment2 first push

---
+++ script2.js
@@ -0,0 +1,13 @@
+let str = "";
+let vowels = ['A','E','I','O','U','a','e','i','o','u'];
+
+
+if (str.slice[0,1] = vowels) {
+  document.write(str + 'way')
+} else if (str.slice[0,1] != vowels){
+  document.write('')
+} else if str.slice[0,2]{
+  document.write('')
+} else {
+  document.write()
+}
