eb11224c559a453f635c995bda85af3c49974422
rldettloff <114546927+rldettloff@users.noreply.github.com>
2023-01-15 09:30:22 +0000

    code updated

--- script2.js
+++ script2.js
@@ -1,13 +1,21 @@
-let str = "";
-let vowels = ['A','E','I','O','U','a','e','i','o','u'];
+let pigWord = prompt('Enter Word')
+let vowels = ('AEIOUaeiou')
+let consonent = ('BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz')
 
-
-if (str.slice[0,1] = vowels) {
-  document.write(str + 'way')
-} else if (str.slice[0,1] != vowels){
-  document.write('')
-} else if str.slice[0,2]{
-  document.write('')
-} else {
-  document.write()
-}
+    if pigWord = ('', ' ',){
+        document.write('input cannot be translated');
+      
+    } else if (vowels.includes(pigWord(0))) {
+      document.write(pigWord ++ 'way');
+      
+    } else if (consonent.includes(pigWord(0))) {
+      let halfWord = pigWord.slice(0);
+      let halfWord2 = pigWord.slice(-1);
+      document.write(halfWord + halfword2 ++ 'ay');
+      
+    } else if (consonent.includes(pigWord(0,1))) {
+      let halfWord = pigWord.slice(0,1);
+      let halfWord2 = pigWord.slice(-2);
+      document.write(halfWord + halfword2 ++ 'ay');
+    }
+    
